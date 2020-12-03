d850ba9c15cddd1fed7c6890917e9874d465f505
omon4412 <vetrov241201@yandex.ru>
2020-12-03 20:39:21 +0300

    Создал структуру Forest с полями

--- rwFunc.hpp
+++ rwFunc.hpp
@@ -1,6 +1,16 @@
 #ifndef RWFUNC_HPP
 #define RWFUNC_HPP
 
+#include <iostream>
+
+struct Forest
+{
+    int id;
+    int lotNumber;
+    std::string name;
+    double square;
+};
+
 void ReadFile();
 void WriteFile();
 
